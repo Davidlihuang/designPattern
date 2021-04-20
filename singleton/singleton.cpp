8e57303258844ed04febb5c8e276ff9be733ac3e
huangli-linux <huanglixianhui@163.com>
2021-04-20 21:37:03 +0800

    add more tips

--- singleton/singleton.cpp
+++ singleton/singleton.cpp
@@ -31,7 +31,52 @@ Singleton* Singleton::getInstance()
     return m_instance;
 }
 
+/*
+//线程安全
+Singleton* Singleton::getInstance()
+{
+   Lock lock;
+   if(nullptr == m_instance)
+   {
+       m_instance = new Singleton;
+   }
+   return m_instance;
+}
+//双检查锁
+Singleton* Singleton::getInstance()
+{
+   if(nullptr == m_instance)
+   {
+      Lock lock;
+      if(nullptr == m_instance)
+      {
+          m_instance = new Singleton;
+      }
+   }
+   return m_instance;
+}
+//c++11跨平台
+std::atomic<Singleton*> SingleTon::m_instance;
+std::mutex Singleton::m_mutex;
 
+Singleton* Singleton::getInstance()
+{
+    Singleton* tmp = m_instance.load(std::memory_order_relaxed);
+    std::atomic_thread_fence(std::memory_order_acquire);// 获取内存fence,防止reorder
+    if(tmp == nullptr)
+    {
+       std::lock_guard<std::mutex> lock(m_mutex);
+       tmp = m_instance.load(std::memory_order_relaxed);
+       if(tmp == nullptr)
+       {
+           tmp = new Singleton;
+	   std::atomic_thread_fence(std::memory_order_release);//释放内存fence
+	   m_instance.store(tmp,std::memory_order_relaxed);
+       }
+    }
+    return tmp;
+}
+*/
 int main()
 {
     Singleton* instance0 = Singleton::getInstance();
